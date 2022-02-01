6fb2ddbedd95cdb102d540c385b9742ba3423fe3
abu <abu@gmail.com>
2022-02-02 02:07:48 +0800

    update

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	log: { level: 'trace' },
+	http: [
+		{
+			name: '',
+			port: ,
+		},
+	],
+	token: {
+		host: '',
+		port: ,
+	},
+	redis: [
+		{
+			name: 'redis',
+			host: '',
+			port: ,
+		},
+	],
+	db: [
+		{
+			name: '',
+			host: '',
+			port: ,
+			user: '',
+			password: '',
+			database: '',
+		},
+	],
+	ws: [{ name: 'ws', port:  }],
+}
